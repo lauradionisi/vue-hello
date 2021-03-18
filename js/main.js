85eada3c77a2142cbd37542aea1a4f3702e2e229
Laura <laura.dionisi91@gmail.com>
2021-03-18 17:03:31 +0100

    Vue Hello

---
+++ js/main.js
@@ -0,0 +1,13 @@
+var app = new Vue (
+    {
+
+    el:"#app",
+
+    data: {
+        colorClass: "color_size",
+        message: "Esercizio Vue",
+        message2: "Vue Hello",
+        image: "img/cascata.jpg"
+    },
+    
+});
